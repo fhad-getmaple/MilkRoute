750b5eca172c5427714cd8c9798fb9d7c7929b98
Fhad Sheikh <fhad@getmaple.ca>
2017-03-21 00:00:03 -0400

    fixed eslint issues

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-import App from './App'
+import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
-  <AppContainer>
-    <App/>
-  </AppContainer>,
-  document.getElementById('root')
+    <AppContainer>
+        <App />
+    </AppContainer>,
+    document.getElementById('root'), //eslint-disable-line
 );
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp/>
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default; //eslint-disable-line
+        ReactDOM.render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            document.getElementById('root'), //eslint-disable-line
+        );
+    });
 }
